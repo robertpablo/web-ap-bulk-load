7f74f5c559947dce5bd265f19e24b441661166c4
robert pablo <robertbarrera@outlook.com>
2024-06-15 11:25:53 -0500

    fix: avance de botones-shared

--- src/app/features/bulk-load-management/pages/bulk-load-list/components/bulk-load-button/bulk-load-button.component.ts
+++ src/app/features/bulk-load-management/pages/bulk-load-list/components/bulk-load-button/bulk-load-button.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import {
+  RpbjBaseButtonComponent,
+  RpbjButtonNewComponent,
+} from '@ropabajo/shared/components';
 
 @Component({
   selector: 'rpbj-bulk-load-button',
   templateUrl: './bulk-load-button.component.html',
   standalone: true,
+  imports: [RpbjBaseButtonComponent, RpbjButtonNewComponent],
 })
 export class BulkLoadButtonComponent {}
